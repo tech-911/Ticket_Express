b2ccff0d34d75d5e5220a2baf0e4743884a3c612
tech-911 <babatundeolatunji74@gmail.com>
2023-01-26 11:59:16 +0100

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import footer_design from "../../assets/png/footer_design.png";
-import logo from "../../assets/png/Logo.png";
+import logo from "../../assets/png/footerLogo.png";
 import "./footer.scss";
 const Footer = () => {
   return (
@@ -8,13 +8,32 @@ const Footer = () => {
       <img className="footer_design" src={footer_design} alt="footer_desgin" />
       <div className="footer_container">
         <div className="footer_item_wrap">
-          <div>
-            <img src={logo} alt="logo" />
-            <p>Book your trip in minute, get full Control for much longer.</p>
+          <div className="footer_logo_wrap">
+            <img className="footer_logo" src={logo} alt="logo" />
+            <p className="footer_logo_text">
+              Book your trip in minute, get full Control for much longer.
+            </p>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="footer_rightContent">
+            <ul className="footer_items1_wrap">
+              <li className="footer_item1">Company</li>
+              <li className="footer_item2">About</li>
+              <li className="footer_item3">Carrers</li>
+              <li className="footer_item4">Mobile</li>
+            </ul>
+            <ul className="footer_items2_wrap">
+              <li className="footer_item1">Contact</li>
+              <li className="footer_item2">Help/FAQ</li>
+              <li className="footer_item3">Press</li>
+              <li className="footer_item4">Affilates</li>
+            </ul>
+            <ul className="footer_items3_wrap">
+              <li className="footer_item1">More</li>
+              <li className="footer_item2">Airlines</li>
+              <li className="footer_item3">Train station</li>
+              <li className="footer_item4">Bus station</li>
+            </ul>
+          </div>
         </div>
         <p className="footer_copyright">All rights reserved@ticketxpress.com</p>
       </div>
