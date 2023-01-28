3127361e1d6f600fac2ab160c1bc47bd413d3fd8
tech-911 <babatundeolatunji74@gmail.com>
2023-01-28 22:34:10 +0100

    ...updating

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Landing from "./pages/landing/Landing";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-
+import PageNotFound from "./pages/error/NotFound";
+import CommingSoon from "./pages/error/construction";
 const App = () => {
   return (
     <div className="app_wrapper">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/comming" element={<CommingSoon />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import "./nav.scss";
 import Logo from "../../assets/png/Logo.png";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+import { useState } from "react";
 
 const Nav = () => {
+  const [hamburger, setHamburger] = useState(0);
   return (
     <div className="Nav_container">
+      <div
+        className={`Nav_mobile_menue ${!hamburger ? "Nav_mobile_close" : ""}`}
+      >
+        <GrClose
+          className="Nav_mobile_menue_close_icon"
+          onClick={() => {
+            setHamburger(0);
+          }}
+        />
+        <ul className="Nav_right_lists">
+          <li className="Nav_right_list1">Bus</li>
+          <li className="Nav_right_list2">Train</li>
+          <li className="Nav_right_list3">Flights</li>
+          <li className="Nav_right_list4">Bookings</li>
+          <li className="Nav_right_list5">Contact Us</li>
+        </ul>
+        <button className="Nav_right_login">Login</button>
+      </div>
       <div className="Nav_wrapper">
         <Link to="/" className="Nav_Logo_link">
           <img className="Nav_logo" src={Logo} alt="logo" />
@@ -21,6 +43,12 @@ const Nav = () => {
           </ul>
           <button className="Nav_right_login">Login</button>
         </div>
+        <GiHamburgerMenu
+          onClick={() => {
+            hamburger ? setHamburger(0) : setHamburger(1);
+          }}
+          className="nav_hamburger"
+        />
       </div>
     </div>
   );
