d5bc30712e75294c88c70bcae2c7055bb2c82a64
tech-911 <babatundeolatunji74@gmail.com>
2023-02-10 01:32:21 +0100

    added booking to user

--- src/pages/protected/user/booking/Booking.jsx
+++ src/pages/protected/user/booking/Booking.jsx
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { Label } from "recharts";
 import "./booking.scss";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 const Booking = () => {
+  //---------------Hooks------------------------
+  const [disable, setDisable] = useState(0);
+  let [data, setData] = useState({
+    name: "",
+    email: "",
+    destination: "",
+    passangers_number: 0,
+    time: "",
+    date: "",
+    car_type: "",
+  });
+  const { token } = useSelector((state) => state.login);
+
+  //---------------Variables------------------------
+  const baseUrl = "https://ticketappbackend.vercel.app/api/useraction";
+
+  //---------------methods------------------------
+  const handleSubmit = async (e) => {
+    setDisable(1);
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        `${baseUrl}/bookings`,
+        {
+          name: data.name,
+          email: data.email,
+          destination: data.destination,
+          passangers_number: Number(data.passangers_number),
+          time: data.time,
+          date: data.date,
+          car_type: data.car_type,
+        },
+        { headers: { "auth-token": token } }
+      );
+      toast.success("Trip Created", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } catch (error) {
+      toast.error(`Error: ${error.response.data}`, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setDisable(0);
+    }
+    setData({
+      name: "",
+      email: "",
+      destination: "",
+      passangers_number: 0,
+      time: "",
+      date: "",
+      car_type: "",
+    });
+
+    setDisable(0);
+  };
+
+  const handleInput = (e) => {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+  };
+
+  let { name, email, destination, passangers_number, time, date, car_type } =
+    data;
+
+  //---------------Main Retrun------------------------
+
   return (
     <div className="booking_wrapper">
-      <form className="booking_form">
+      <ToastContainer />
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+        className="booking_form"
+      >
         <div className="booking_name">
           <label htmlFor="name" className="booking_name_label">
             Name:
@@ -14,6 +92,11 @@ const Booking = () => {
             type="text"
             className="booking_name_input"
             placeholder="Enter Name"
+            required
+            value={name}
+            onChange={(e) => {
+              handleInput(e);
+            }}
           />
         </div>
         <div className="booking_email">
@@ -22,9 +105,14 @@ const Booking = () => {
           </label>
           <input
             id="email"
-            type="text"
+            type="email"
             className="booking_email_input"
             placeholder="Enter Email"
+            required
+            value={email}
+            onChange={(e) => {
+              handleInput(e);
+            }}
           />
         </div>
         <div className="booking_destination">
@@ -41,8 +129,13 @@ const Booking = () => {
             name="destination"
             id="destination"
             className="booking_destination_input"
+            required
+            value={destination}
+            onChange={(e) => {
+              handleInput(e);
+            }}
           >
-            <option className="placeholder" value="" selected="selected">
+            <option className="placeholder" value="" defaultValue={true}>
               Select Destination
             </option>
             <option value="Abia">Abia</option>
@@ -85,14 +178,19 @@ const Booking = () => {
           </select>
         </div>
         <div className="booking_no">
-          <label htmlFor="no" className="booking_no_label">
+          <label htmlFor="passangers_number" className="booking_no_label">
             No of Passangers:
           </label>
           <input
             placeholder="Enter number of passangers"
-            id="no"
+            id="passangers_number"
             type="number"
             className="booking_no_input"
+            required
+            value={passangers_number}
+            onChange={(e) => {
+              handleInput(e);
+            }}
           />
         </div>
         <div className="booking_more_wrapper">
@@ -100,20 +198,47 @@ const Booking = () => {
             <label htmlFor="time" className="booking_time_label">
               Time:
             </label>
-            <input id="time" type="time" className="booking_time_input" />
+            <input
+              id="time"
+              type="time"
+              className="booking_time_input"
+              required
+              value={time}
+              onChange={(e) => {
+                handleInput(e);
+              }}
+            />
           </div>
           <div className="booking_date">
             <label htmlFor="date" className="booking_date_label">
               Date:
             </label>
-            <input id="date" type="date" className="booking_date_input" />
+            <input
+              id="date"
+              type="date"
+              className="booking_date_input"
+              required
+              value={date}
+              onChange={(e) => {
+                handleInput(e);
+              }}
+            />
           </div>
           <div className="booking_car">
-            <label htmlFor="car" className="booking_car_label">
+            <label htmlFor="car_type" className="booking_car_label">
               Car Type:
             </label>
-            <select name="car" id="car" className="booking_car_input">
-              <option className="placeholder" value="" selected="selected">
+            <select
+              name="car_type"
+              id="car_type"
+              className="booking_car_input"
+              required
+              value={car_type}
+              onChange={(e) => {
+                handleInput(e);
+              }}
+            >
+              <option className="placeholder" value="" defaultValue={true}>
                 Select Cartype
               </option>
               <option value="Bus">Bus</option>
@@ -123,7 +248,9 @@ const Booking = () => {
             </select>
           </div>
         </div>
-        <button className="booking_submit">Submit</button>
+        <button disabled={disable} className="booking_submit">
+          {disable ? "Submitting..." : "Submit"}
+        </button>
       </form>
     </div>
   );
